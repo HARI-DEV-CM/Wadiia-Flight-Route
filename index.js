9a98db1a2ebccb2023d2b10f629915f279b6439e
santhosh162000 <santhosh.m@codingmart.com>
2021-10-12 12:49:24 +0530

    Updated form section

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const departure = document.getElementById("departure");
-const departure_mob = document.getElementById("departure-mob");
 const arrival = document.getElementById("arrival");
-const arrival_mob = document.getElementById("arrival-mob");
 const faqsContainer = document.querySelector("#faqContainer");
 let faqsTitle = document.querySelector("#faqHeading");
-
 
 $(function getdeparture() {
   $.getJSON("https://wadiia-backend.herokuapp.com/departure", function (data) {
@@ -12,7 +9,7 @@ $(function getdeparture() {
     for (var i = 0, len = data.length; i < len; i++) {
       data[i].value && autoComplete.push(data[i].value);
     }
-    $("#departure, #departure-mob").autocomplete({
+    $("#departure").autocomplete({
       source: autoComplete,
       minLength: 2,
       autoFocus: true,
@@ -26,11 +23,45 @@ $(function getarrival() {
     for (var i = 0, len = data.length; i < len; i++) {
       data[i].value && autoComplete.push(data[i].value);
     }
-    $("#arrival, #arrival-mob").autocomplete({
+    $("#arrival").autocomplete({
       source: autoComplete,
       minLength: 2,
       autoFocus: true,
     });
+  });
+});
+
+$(function () {
+  $("#departure-date").datepicker({
+    dateFormat: "dd-mm-yy",
+    minDate: new Date(),
+    yearRange:
+      new Date().getFullYear().toString() +
+      ":" +
+      new Date().getFullYear().toString(),
+    onClose: function (selectedDate) {
+      $("#arrival-date").datepicker(
+        "option",
+        "minDate",
+        selectedDate,
+        new Date()
+      );
+    },
+  });
+  $("#arrival-date").datepicker({
+    dateFormat: "dd-mm-yy",
+    minDate: new Date(),
+    yearRange:
+      new Date().getFullYear().toString() +
+      ":" +
+      new Date().getFullYear().toString(),
+    onClose: function (selectedDate) {
+      $("#departure-date").datepicker(
+        "option",
+        "maxDate",
+        selectedDate
+      );
+    },
   });
 });
 
@@ -51,49 +82,7 @@ const getcms = async (data) => {
   console.log({ body });
   const faqs = JSON.parse(body.data.faq_object);
 
-  faqsTitle.innerHTML = `Frequently Asked Questions - ${body.data.airline_name} flights from ${body.data.source} to ${body.data.destination}`
-  // console.log(faqsTitle.innerHTML, "hhhh");
-
-  let faqsComponent = ``;
-
-  if (faqs[0]) {
-    for (const faq of faqs) {
-      const faqComponent = `
-          <button class="accordion1">
-            ${faq.question.trim()}
-            <i class="fa fa-plus" aria-hidden="true"></i>
-          </button>
-          <div class="panel1">
-            <p> ${faq.answer.trim()} </p>
-          </div>`;
-      faqsComponent += faqComponent.trim();
-    }
-  }
-  faqsContainer.innerHTML = faqsComponent;
-  addAccordianEventListener();
-  console.log({ faqsComponent });
-};
-// getcms();
-
-//MOBILE
-const getcmsmob = async (data) => {
-  console.log(data, "cms");
-  const response = await fetch("https://wadiia-backend.herokuapp.com/route", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      departure: departure_mob.value,
-      arrival: arrival_mob.value,
-    }),
-  });
-
-  const body = await response.json();
-  console.log({ body });
-  const faqs = JSON.parse(body.data.faq_object);
-
-  faqsTitle.innerHTML = `Frequently Asked Questions - ${body.data.airline_name} flights from ${body.data.source} to ${body.data.destination}`
+  faqsTitle.innerHTML = `Frequently Asked Questions - ${body.data.airline_name} flights from ${body.data.source} to ${body.data.destination}`;
   // console.log(faqsTitle.innerHTML, "hhhh");
 
   let faqsComponent = ``;
@@ -116,89 +105,80 @@ const getcmsmob = async (data) => {
   console.log({ faqsComponent });
 };
 
+//Form Section
+
+var trip = "Round-Trip";
 
 $(function () {
-  $("#departure-date, #departure-date-mob").datepicker({
-    dateFormat: "dd-mm-yy",
-    minDate: new Date(),
-    yearRange:
-      new Date().getFullYear().toString() +
-      ":" +
-      new Date().getFullYear().toString(),
-    onClose: function (selectedDate) {
-      $("#arrival-date, #arrival-date-mob").datepicker(
-        "option",
-        "minDate",
-        selectedDate,
-        new Date()
-      );
-    },
-  });
-  $("#arrival-date, #arrival-date-mob").datepicker({
-    dateFormat: "dd-mm-yy",
-    minDate: new Date(),
-    yearRange:
-      new Date().getFullYear().toString() +
-      ":" +
-      new Date().getFullYear().toString(),
-    onClose: function (selectedDate) {
-      $("#departure-date, #departure-date-mob").datepicker(
-        "option",
-        "maxDate",
-        selectedDate
-      );
-    },
+  $('input:radio[name="radio-button"]').change(function () {
+    if ($(this).val() == "One-Way") {
+      document.getElementById("arrival-div").classList.add("wad-desktop-oneway-arr");
+      var third_row_input = document.getElementsByClassName("wad-third-row-input");
+      for(var i = 0; i < third_row_input.length; i++)
+      {
+          third_row_input[i].classList.add('wad-desktop-oneway-dep');
+          console.log(third_row_input[i].className);
+      }
+      trip = "One-Way";
+    } else if ($(this).val() == "Round-Trip") {
+      document.getElementById("arrival-div").classList.remove("wad-desktop-oneway-arr");
+      var third_row_input = document.getElementsByClassName("wad-third-row-input");
+      for(var i = 0; i < third_row_input.length; i++)
+      {
+          third_row_input[i].classList.remove('wad-desktop-oneway-dep');
+          console.log(third_row_input[i].className);
+      }
+      trip = "Round-Trip";
+    } else {
+      document.getElementById("arrival-div").classList.remove("wad-desktop-oneway-arr");
+      var third_row_input = document.getElementsByClassName("wad-third-row-input");
+      for(var i = 0; i < third_row_input.length; i++)
+      {
+          third_row_input[i].classList.remove('wad-desktop-oneway-dep');
+          console.log(third_row_input[i].className);
+      }
+      trip = "Multi-Trip";
+    }
   });
 });
+
+// Mobile Button Functionality
+
+$("#onewaybutton").click(function () {
+  document.getElementById("arrival-div").classList.add("wad-mob-oneway-arr");
+  document.getElementById("onewaybutton").classList.add("wad-button-clicked");
+  document.getElementById("roundtripbutton").classList.remove("wad-button-clicked");
+  document.getElementById("multicitybutton").classList.remove("wad-button-clicked");
+  trip = "One-Way";
+  console.log(trip);
+});
+
+$("#roundtripbutton").click(function () {
+  document.getElementById("arrival-div").classList.remove("wad-mob-oneway-arr");
+  document.getElementById("onewaybutton").classList.remove("wad-button-clicked");
+  document.getElementById("roundtripbutton").classList.add("wad-button-clicked");
+  document.getElementById("multicitybutton").classList.remove("wad-button-clicked");
+  trip = "Round-Trip";
+});
+
+$("#multicitybutton").click(function () {
+  document.getElementById("arrival-div").classList.remove("wad-mob-oneway-arr");
+  document.getElementById("onewaybutton").classList.remove("wad-button-clicked");
+  document.getElementById("roundtripbutton").classList.remove("wad-button-clicked");
+  document.getElementById("multicitybutton").classList.add("wad-button-clicked");
+  trip = "Multi-Trip";
+});
+
 
 // Redirecting URL
 
 function redirectURL() {
-  var val = document.getElementById("class").value;
-  var opts = document.getElementById("journeyclass").childNodes;
-  for (var i = 0; i < opts.length; i++) {
-    if (opts[i].value === val) {
-      var trip = document.querySelector(
-        'input[name="radio-button"]:checked'
-      ).value;
-      var departure = document.getElementById("departure").value;
-      var arrival = document.getElementById("arrival").value;
-      var fromdate = document.getElementById("departure-date").value;
-      var todate = document.getElementById("arrival-date").value;
-      var traveller = document.getElementById("travellers").value;
-      var journeyclass = document.getElementById("class").value;
-
-      window.open(
-        "../../flights/" +
-          "?trip=" +
-          trip +
-          "&sourceCity=" +
-          departure +
-          "&destinationCity=" +
-          arrival +
-          "&fromDate=" +
-          fromdate +
-          "&toDate=" +
-          todate +
-          "&travellers=" +
-          traveller +
-          "&class=" +
-          journeyclass
-      );
-
-      return false;
-    }
-  }
-}
-
-function redirectMobURL() {
-  var trip = document.getElementById("hiddenbutton").value;
-  var departure = document.getElementById("departure-mob").value;
-  var arrival = document.getElementById("arrival-mob").value;
-  var fromdate = document.getElementById("departure-date-mob").value;
-  var todate = document.getElementById("arrival-date-mob").value;
-  var traveller = document.getElementById("travellers-mob").value;
-  var journeyclass = document.getElementById("class-mob").value;
+  var departure = document.getElementById("departure").value;
+  var arrival = document.getElementById("arrival").value;
+  var fromdate = document.getElementById("departure-date").value;
+  var todate = document.getElementById("arrival-date").value;
+  var traveller = document.getElementById("travellers").value;
+  var journeyclass = document.getElementById("class").value;
 
   window.open(
     "../../flights/" +
@@ -220,56 +200,3 @@ function redirectMobURL() {
 
   return false;
 }
-
-$(function () {
-  $('input:radio[name="radio-button"]').change(function () {
-    if ($(this).val() == "One-Way") {
-      document.getElementById("arrival-icon").style.display = "none";
-      document.getElementById("arrival-date").style.display = "none";
-      document.getElementById("spanarrival").style.display = "none";
-      document.getElementById("departure-date").style.width = "286px"
-    } else if ($(this).val() == "Round-Trip") {
-      document.getElementById("departure-date").style.width = "101px";
-      document.getElementById("arrival-date").style.display = "inline-block";
-      document.getElementById("spanarrival").style.display = "inline-block";
-      document.getElementById("arrival-icon").style.display = "inline-block";
-    } else {
-      document.getElementById("departure-date").style.width = "101px";
-      document.getElementById("arrival-date").style.display = "inline-block";
-      document.getElementById("spanarrival").style.display = "inline-block";
-      document.getElementById("arrival-icon").style.display = "inline-block";
-    }
-  });
-});
-
-// Mobile Button Functionality
-
-$("#onewaybutton").click(function () {
-  $("#hiddenbutton").val("One-Way");
-  document.getElementById("arrival-icon-mob").style.display = "none";
-  document.getElementById("arrival-date-mob").style.display = "none";
-  document.getElementById("spanarrivalmob").style.display = "none";
-  document.getElementById("onewaybutton").classList.add("wad-button-clicked");
-  document.getElementById("roundtripbutton").classList.remove("wad-button-clicked");
-  document.getElementById("multicitybutton").classList.remove("wad-button-clicked");
-});
-
-$("#roundtripbutton").click(function () {
-  $("#hiddenbutton").val("Round-Trip");
-  document.getElementById("arrival-date-mob").style.display = "inline-block";
-  document.getElementById("arrival-icon-mob").style.display = "inline-block";
-  document.getElementById("spanarrivalmob").style.display = "inline-block";
-  document.getElementById("onewaybutton").classList.remove("wad-button-clicked");
-  document.getElementById("roundtripbutton").classList.add("wad-button-clicked");
-  document.getElementById("multicitybutton").classList.remove("wad-button-clicked");
-});
-
-$("#multicitybutton").click(function () {
-  $("#hiddenbutton").val("Multi-Trip");
-  document.getElementById("arrival-date-mob").style.display = "inline-block";
-  document.getElementById("arrival-icon-mob").style.display = "inline-block";
-  document.getElementById("spanarrivalmob").style.display = "inline-block";
-  document.getElementById("onewaybutton").classList.remove("wad-button-clicked");
-  document.getElementById("roundtripbutton").classList.remove("wad-button-clicked");
-  document.getElementById("multicitybutton").classList.add("wad-button-clicked");
-});
